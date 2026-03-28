82380bb5407b0eeefe38676ecf89fe2e73bf3e12
sebastos1 <sjallabong@gmail.com>
2026-03-28 18:05:51 +0100

    flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "ello";
+  outputs =
+    { self, nixpkgs }:
+    {
+      packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.stdenvNoCC.mkDerivation {
+        name = "ello-plymouth";
+        src = self;
+        installPhase = ''
+          mkdir -p $out/share/plymouth/themes/ello
+          cp -r * $out/share/plymouth/themes/ello/
+        '';
+      };
+    };
+}
